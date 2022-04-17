b3f42e9bfb8a07d5ce0ff5c48c9cb0405060cea2
ifthis <zuiidea@outlook.com>
2022-04-17 14:02:05 +0800

    ✨  adding ThirdPersonCamera lookAt test

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,73 @@
-import { Editor, FirstPersonCamera, Joystick, Model, Mouse, Reticle, Skybox, useKeyboard, useLoop, usePreload, useSpring, World, useWindowSize } from "lingo3d-react"
-import { useRef, useState } from "react"
+import { ThirdPersonCamera, Model, OrbitCamera, Skybox, usePreload, World, Cube } from 'lingo3d-react'
+import { useRef, useState } from 'react'
 
 const Game = () => {
-  const key = useKeyboard()
-  const [joystick, setJoystick] = useState({ x: 0, y: 0, angle: 0 })
-
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 })
   const characterRef = useRef()
-  const motion = (key === "w" || joystick.y < 0) ? "walking" : "idle"
-  
-  useLoop(() => {
-    characterRef.current.moveForward(-10)
-  }, motion === "walking")
 
-  const gunSpring = useSpring({ from: -30, to: -40 })
-
-  const windowSize = useWindowSize()
-  const zoom = windowSize.width > windowSize.height ? 1 : 0.5
-
-  return (<>
-    <World logarithmicDepth>
-      <Model src="Grassland.glb" scale={270} physics="map" />
-      <FirstPersonCamera active mouseControl zoom={zoom}>
+  return (
+    <>
+      <World logarithmicDepth>
+        <Model
+          src="Grassland.glb"
+          scale={270}
+          physics="map"
+          onClick={(e) => {
+            setPosition(e.point)
+            characterRef.current.lookAt(e.point)
+          }}
+        />
+        {/* <OrbitCamera active /> */}
+        <ThirdPersonCamera active>
           <Model
             ref={characterRef}
-            src="Fox.fbx" 
+            src="Fox.fbx"
             physics="character"
-            animations={{ idle:"Rifle Idle.fbx", walking:"Rifle Run.fbx" }}
-            animation={motion}
-            visible={false}
+            animations={{ idle: 'Rifle Idle.fbx', walking: 'Rifle Run.fbx' }}
           />
-          <Model src="gun.glb" z={gunSpring} x={20} y={-10} scale={0.2} innerRotationY={-90} />
-      </FirstPersonCamera>
-      <Skybox texture="skybox.jpg" />
-    </World>
-    <Mouse onClick={() => gunSpring.restart()} />
-    <Joystick onMove={e => setJoystick(e)} />
-    <Reticle variant={7} color="white" />
-  </>)
+        </ThirdPersonCamera>
+        <Skybox texture="skybox.jpg" />
+        <Cube color="orange" x={position.x} y={position.y} z={position.z}></Cube>
+      </World>
+    </>
+  )
 }
 
 const App = () => {
-  const progress = usePreload([
-    "Fox.fbx",
-    "Grassland.glb",
-    "ground.jpeg",
-    "gun.glb",
-    "Idle.fbx",
-    "Rifle Idle.fbx",
-    "Rifle Run.fbx",
-    "skybox.jpg",
-    "Walking.fbx"
-  ], "6.6mb")
+  const progress = usePreload(
+    [
+      'Fox.fbx',
+      'Grassland.glb',
+      'ground.jpeg',
+      'Idle.fbx',
+      'Rifle Idle.fbx',
+      'Rifle Run.fbx',
+      'skybox.jpg',
+      'Walking.fbx',
+    ],
+    '6.6mb',
+  )
 
   if (progress < 100)
     return (
-      <div style={{
-        width: "100vw",
-        height: "100vh",
-        left: 0,
-        top: 0,
-        backgroundColor: "black",
-        color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-      }}>
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          left: 0,
+          top: 0,
+          backgroundColor: 'black',
+          color: 'white',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         loading {Math.round(progress)}%
       </div>
     )
 
-  return (
-    <Game />
-  )
+  return <Game />
 }
 
 export default App
